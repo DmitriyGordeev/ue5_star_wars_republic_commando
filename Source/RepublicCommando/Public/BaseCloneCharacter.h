8c1a61bbd1032b6d0117e9b1affc499469119ee9
Dimaster <gordeev.dmitriy@physics.msu.ru>
2023-07-12 11:22:20 +0300

    Base character classes (C++)

---
+++ Source/RepublicCommando/Public/BaseCloneCharacter.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseNPCCharacter.h"
+#include "BaseCloneCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REPUBLICCOMMANDO_API ABaseCloneCharacter : public ABaseNPCCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseCloneCharacter();
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite)
+	bool InCover;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite)
+	AActor* TargetEnemy;
+};
---
+++ Source/RepublicCommando/Public/BaseNPCCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RCCharacter.h"
+#include "BaseNPCCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class REPUBLICCOMMANDO_API ABaseNPCCharacter : public ARCCharacter
+{
+	GENERATED_BODY()
+	
+};
